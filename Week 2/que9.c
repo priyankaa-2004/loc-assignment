0e988d3b778086b816419914398a2e9670a9cbc8
priyankaa-2004 <127417192+priyankaa-2004@users.noreply.github.com>
2023-03-26 17:37:54 +0530

    Add files via upload

---
+++ Week 2/que9.c
@@ -0,0 +1,13 @@
+//print the star pattern
+#include <stdio.h>
+void main() 
+{
+   for(int i = 5; i > 0; i--)
+   {
+       for(int j = 0; j < i; j++)
+       {
+           printf("*");
+       }
+       printf("\n");
+   }
+}
